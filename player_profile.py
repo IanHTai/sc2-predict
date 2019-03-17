633425080c01feb8fb087e60e715581761645321
IanHTai <IanHTai@users.noreply.github.com>
2019-03-17 02:53:12 -0700

    fixed no return statement on glickoF

--- player_profile.py
+++ player_profile.py
@@ -45,7 +45,32 @@ class PlayerProfile:
 
         playTimeGap = date - self.lastPlayedDate
         self.expAverageLastPlayed = self.expAlpha * playTimeGap + self.expAverageLastPlayed * (1 - self.expAlpha)
-        
+
+
+    def updateGlicko(self, opponentRating, opponentRD, win):
+        winNum  = 1 if win else 0
+        mu = (self.glickoRating - 1500) / 173.7178
+        phi = self.glickoRD / 173.7178
+        muOpponent = (opponentRating - 1500) / 173.7178
+        phiOpponent = opponentRD / 173.7178
+
+        expected = self.glickoExpected(mu, muOpponent, phiOpponent)
+        v = (self.glickoG(phiOpponent) ** 2 * expected * (1 - expected)) ** -1
+        preMultiDelta = self.glickoG(phiOpponent) * (winNum - expected)
+        delta = preMultiDelta * v
+
+        sigmaPrime = self.glickoNewVol(self.glickoVol, delta, phi, v, self.glickoTau)
+
+        phiStar = math.sqrt(phi ** 2 + (sigmaPrime ** 2))
+
+        phiPrime = 1 / math.sqrt((1 / (phiStar ** 2)) + (1 / v))
+        muPrime = mu + phiPrime ** 2 * preMultiDelta
+
+        rPrime = 173.7178 * muPrime + 1500
+        RDPrime = 173.7178 * phiPrime
+
+        self.glickoRating = rPrime
+        self.glickoRD = RDPrime
 
     def placementsEnd(self):
         # GLICKO2, info found here http://www.glicko.net/glicko/glicko2.pdf
@@ -78,31 +103,9 @@ class PlayerProfile:
         self.glickoRating = rPrime
         self.glickoRD = RDPrime
 
-    def updateGlicko(self, opponentRating, opponentRD, win):
-        winNum  = 1 if win else 0
-        mu = (self.glickoRating - 1500) / 173.7178
-        phi = self.glickoRD / 173.7178
-        muOpponent = (opponentRating - 1500) / 173.7178
-        phiOpponent = opponentRD / 173.7178
-
-        expected = self.glickoExpected(mu, muOpponent, phiOpponent)
-        v = (self.glickoG(phiOpponent) ** 2 * expected * (1 - expected)) ** -1
-        preMultiDelta = self.glickoG(phiOpponent) * (winNum - expected)
-        delta = preMultiDelta * v
-
-        sigmaPrime = self.glickoNewVol(self.glickoVol, delta, phi, v, self.glickoTau)
-
-        phiStar = math.sqrt(phi ** 2 + (sigmaPrime ** 2))
-
-        phiPrime = 1 / math.sqrt((1 / (phiStar ** 2)) + (1 / v))
-        muPrime = mu + phiPrime ** 2 * preMultiDelta
-
-        rPrime = 173.7178 * muPrime + 1500
-        RDPrime = 173.7178 * phiPrime
-
-        self.glickoRating = rPrime
-        self.glickoRD = RDPrime
-
+    """
+    Helper functions for Glicko calculations
+    """
     def glickoNewVol(self, sigma, delta, phi, v, tau):
         a = math.log(sigma ** 2)
         A = math.log(sigma ** 2)
@@ -135,13 +138,17 @@ class PlayerProfile:
     def glickoF(self, x, delta, phi, v, a, tau):
         left = (math.e**x * (delta ** 2 - phi ** 2 - v - math.e**x)) / (2 * ((phi ** 2 + v + math.e**x) ** 2))
         right = (x - a) / (tau**2)
-
+        return left - right
 
     def glickoG(self, phi):
         return 1 / (math.sqrt(1 + (3 * (phi**2) / (math.pi ** 2))))
 
     def glickoExpected(self, muSelf, muOpponent, phi):
         return 1 / (1 + math.exp(-self.glickoG(phi) * (muSelf - muOpponent)))
+
+    """
+    End of Glicko functions
+    """
 
     def getFeatures(self):
         return None
